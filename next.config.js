4280cc141f8c0227788dd172402695b4a8eeb587
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-09 16:36:24 +0200

    feat: config, added remote and exposed Footer component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "frontend_two",
+        remotes: {
+          frontend_three: `frontend_three@http://localhost:3002/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry_3.js`,
+          frontend_one: `frontend_one@http://localhost:3000/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry_2.js",
+        exposes: {
+          "./footer": "./src/components/Footer/index.tsx",
+        },
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
